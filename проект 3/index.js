8ed68ced815c8d1a3d59a5b9e532b8893e46dfe1
skkdh <138329964+skkdh@users.noreply.github.com>
2025-02-20 15:15:02 +0200

    Add files via upload

---
+++ проект 3/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const burger = document.querySelector(".burger");
+    const menu = document.querySelector(".menu");
+  
+    burger.addEventListener("click", function () {
+      menu.classList.toggle("active");
+    });
+  });
